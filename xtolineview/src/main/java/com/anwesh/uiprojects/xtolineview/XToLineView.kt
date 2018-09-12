b73b9e90d2e83722145fc46525dcf5cb49e25d4e
Anwesh43 <anweshthecool0@gmail.com>
2018-09-12 10:41:13 +0530

    adding xtolineview module

---
+++ xtolineview/src/main/java/com/anwesh/uiprojects/xtolineview/XToLineView.kt
@@ -0,0 +1,13 @@
+package com.anwesh.uiprojects.xtolineview
+
+/**
+ * Created by anweshmishra on 12/09/18.
+ */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
