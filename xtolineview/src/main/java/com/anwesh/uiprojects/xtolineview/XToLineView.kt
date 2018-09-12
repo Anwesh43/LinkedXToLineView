93d3e2b109794959d50bc1badf269e35e2f9f21b
Anwesh43 <anweshthecool0@gmail.com>
2018-09-12 11:01:54 +0530

    adding LinkedXToLine class

--- xtolineview/src/main/java/com/anwesh/uiprojects/xtolineview/XToLineView.kt
+++ xtolineview/src/main/java/com/anwesh/uiprojects/xtolineview/XToLineView.kt
@@ -140,4 +140,26 @@ class XToLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedXToLine(var i : Int) {
+        private var curr : XTLNode = XTLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
