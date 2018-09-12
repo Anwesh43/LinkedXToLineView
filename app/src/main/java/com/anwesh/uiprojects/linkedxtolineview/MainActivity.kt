555fcf184e5866e90c036042e7fe4efba8bbb6cd
Anwesh43 <anweshthecool0@gmail.com>
2018-09-12 11:12:49 +0530

    adding XToLineView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedxtolineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedxtolineview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedxtolineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.xtolineview.XToLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        XToLineView.create(this)
     }
 }
